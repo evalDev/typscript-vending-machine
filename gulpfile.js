6166e7ed24b4e6a199bde3de3391f6663be751ce
evalDev <whatagit@developingdan.com>
2016-08-16 22:30:18 +0100

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    jasmine = require('gulp-jasmine'),
+    tslint = require('gulp-tslint'),
+    typescript = require('gulp-typescript'),
+    concat = require('gulp-concat');
+
+gulp.task('ts', function(){
+  gulp.src('src/js-typescript/*.ts')
+    .pipe(plumber())
+    .pipe(tslint({
+      formatter: 'verbose',
+      configuration: 'tslint.json'
+    }))
+    .pipe(tslint.report({
+        emitError: true
+    }))
+    .pipe(typescript({
+        noImplicitAny: true,
+        out: 'main.js'
+    }))
+    .pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('build-specs', function(){
+  gulp.src('src/specs-jasmine/*.ts')
+    .pipe(plumber())
+    .pipe(tslint({
+      formatter: 'verbose',
+      configuration: 'tslint.json'
+    }))
+    .pipe(tslint.report({
+        emitError: true
+    }))
+    .pipe(typescript({
+        noImplicitAny: true,
+        out: 'projectSpec.js'
+    }))
+    .pipe(gulp.dest('builds/development/specs'))
+  });
+gulp.task('jasmine', function(){
+  return gulp.src('builds/development/specs/mainSpec.js')
+    .pipe(jasmine())
+});
+
+gulp.task('concat-spec', function(){
+  return gulp.src(['builds/development/js/main.js','builds/development/specs/projectSpec.js'])
+    .pipe(concat({
+      path: 'mainSpec.js'
+    }))
+    .pipe(gulp.dest('builds/development/specs/'))
+})
+
+gulp.task('watch', function(){
+  gulp.watch('src/js-typescript/*.ts', ['ts']);
+  gulp.watch('src/specs-jasmine/*.ts', ['build-specs', 'concat-spec', 'jasmine']);
+  // gulp.watch('builds/development/specs/mainSpec.js', ['jasmine']);
+});
+
+gulp.task('default', ['watch'], function(){
+
+});
